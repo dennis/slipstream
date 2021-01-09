4e4f18a93d1a5a971cc2d564f79bdbfc86a006ff
Dennis Møllegaard <dennis@moellegaard.dk>
2021-01-09 23:49:52 +0100

    Move lua function print to UIMethodCollection
    .. as it is related to UI and implemented by MainWindow

--- Backend/Services/LuaServiceLib/UIMethodCollection.cs
+++ Backend/Services/LuaServiceLib/UIMethodCollection.cs
@@ -11,29 +11,38 @@ namespace Slipstream.Backend.Services.LuaServiceLib
         {
             private readonly IEventBus EventBus;
             private readonly IEventFactory EventFactory;
+            private readonly string Prefix;
 
-            public static UIMethodCollection Register(IEventBus eventBus, IEventFactory eventFactory, Lua lua)
+            public static UIMethodCollection Register(IEventBus eventBus, IEventFactory eventFactory, string logPrefix, Lua lua)
             {
-                var m = new UIMethodCollection(eventBus, eventFactory);
+                var m = new UIMethodCollection(eventBus, eventFactory, logPrefix);
 
                 m.Register(lua);
 
                 return m;
             }
 
-            public UIMethodCollection(IEventBus eventBus, IEventFactory eventFactory)
+            public UIMethodCollection(IEventBus eventBus, IEventFactory eventFactory, string logPrefix)
             {
                 EventBus = eventBus;
                 EventFactory = eventFactory;
+                Prefix = logPrefix;
             }
 
             public void Register(Lua lua)
             {
                 lua["ui"] = this;
                 lua.DoString(@"
+function print(s); ui:print(s); end
 function create_button(a); ui:create_button(a); end
 function delete_button(a); ui:delete_button(a); end
 ");
+            }
+
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "This is expose in Lua, so we want to keep that naming style")]
+            public void print(string s)
+            {
+                EventBus.PublishEvent(EventFactory.CreateUICommandWriteToConsole($"{Prefix}: {s}"));
             }
 
             [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "This is expose in Lua, so we want to keep that naming style")]

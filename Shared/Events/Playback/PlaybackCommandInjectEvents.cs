c3c3e9c365ded0ca7fcb3ed47905706ab538c206
Dennis Møllegaard <dennis@moellegaard.dk>
2021-01-15 15:46:54 +0100

    Tweak PlaybackCommand* events
    - Proper initialization of Filename
    - Remove warnings
    - Corrected event type for PlaybackCommandSaveEvents

--- Shared/Events/Playback/PlaybackCommandInjectEvents.cs
+++ Shared/Events/Playback/PlaybackCommandInjectEvents.cs
@@ -6,8 +6,8 @@ namespace Slipstream.Shared.Events.Playback
     {
         public string EventType => "PlaybackCommandInjectEvents";
         public bool ExcludeFromTxrx => true;
-        public string Filename { get; set; }
         public ulong Uptime { get; set; }
+        public string Filename { get; set; } = string.Empty;
 
         public override bool Equals(object obj)
         {
@@ -20,9 +20,9 @@ namespace Slipstream.Shared.Events.Playback
         public override int GetHashCode()
         {
             int hashCode = -1549740322;
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(EventType);
-            hashCode = hashCode * -1521134295 + ExcludeFromTxrx.GetHashCode();
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Filename);
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(EventType);
+            hashCode = (hashCode * -1521134295) + ExcludeFromTxrx.GetHashCode();
+            hashCode = (hashCode * -1521134295) + EqualityComparer<string>.Default.GetHashCode(Filename);
             return hashCode;
         }
     }

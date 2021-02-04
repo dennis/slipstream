04088c11fbe9e59b9a96f64f05ca394d8f7b8d23
Lawrence Rabbets <lawrab@users.noreply.github.com>
2021-02-04 16:52:33 +0000

    Adding unit tests (#69)
    * Adding unit test project with example tests
    
    * Renamed test to be more meaningful Adding unit test project with example tests
    
    * Revert "Renamed test to be more meaningful Adding unit test project with example tests"
    
    This reverts commit d862af2716eec15fcc84d487ecf656f9c2b59453.
    
    * Renamed test to be more meaningul
    
    * Adding dotnet test to CI
    
    * Stop rebuild when tests are run
    
    * Removing CI tests

---
+++ Slipstream.UnitTests/Components/IRacing/Plugins/Trackers/CarInfoTrackerTests.cs
@@ -0,0 +1,46 @@
+﻿using iRacingSDK;
+using Moq;
+using Slipstream.Components.IRacing;
+using Slipstream.Components.IRacing.EventFactory;
+using Slipstream.Components.IRacing.Events;
+using Slipstream.Components.IRacing.Plugins.Models;
+using Slipstream.Components.IRacing.Plugins.Trackers;
+using Slipstream.UnitTests.TestData;
+using Xunit;
+
+namespace Slipstream.UnitTests.Components.IRacing.Plugins.Trackers
+{
+    public class CarInfoTrackerTests
+    {
+        [Fact]
+        public void CanInstantiateCarInfoTracker()
+        {
+            // arrange
+            var eventBusMock = new Mock<Shared.IEventBus>();
+            var eventFactoryMock = new Mock<IIRacingEventFactory>();
+
+            // act
+            var sut = new CarInfoTracker(eventBusMock.Object, eventFactoryMock.Object);
+
+            // assert
+            Assert.NotNull(sut);
+        }
+
+        [Theory]
+        [ClassData(typeof(CarInfoDataSampleTestData))]
+        public void HandleRaisesIRacingCarInfo(DataSample dataSample)
+        {
+            // arrange
+            var eventBusMock = new Mock<Shared.IEventBus>();
+            var eventFactory = new IRacingEventFactory();
+            var trackerDataState = new IRacingDataTrackerState();
+            var sut = new CarInfoTracker(eventBusMock.Object, eventFactory);
+
+            // act
+            sut.Handle(dataSample, trackerDataState);
+
+            // assert
+            eventBusMock.Verify(eb => eb.PublishEvent(It.IsAny<IRacingCarInfo>()), Times.Once(), "IRacingCarInfo event not raised");
+        }
+    }
+}

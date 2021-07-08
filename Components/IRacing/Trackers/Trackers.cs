02316388d68672c4beb8537fb48accdf5e00ba1e
Dennis Møllegaard <dennis@moellegaard.dk>
2021-07-09 00:23:49 +0200

    Bugfix: IRacingThread crash.
    If IRacing isn't running, but you send a:
    IRacingCommandSendCarInfo / SendTrackInfo / SendTrackInfo / WeatherInfo / SessionState / SendRaceFlags event, it would cause IRacingThead
    to crash.

--- Components/IRacing/Trackers/Trackers.cs
+++ Components/IRacing/Trackers/Trackers.cs
@@ -1,4 +1,6 @@
-﻿using Slipstream.Components.IRacing.Models;
+﻿#nullable enable
+
+using Slipstream.Components.IRacing.Models;
 using Slipstream.Shared;
 using System.Collections.Generic;
 using static Slipstream.Components.IRacing.Trackers.IIRacingDataTracker;
@@ -34,8 +36,11 @@ namespace Slipstream.Components.IRacing.Trackers
                 t.Handle(currentState, TrackerState, envelope);
         }
 
-        public void Request(GameState.IState currentState, IEventEnvelope envelope, RequestType type)
+        public void Request(GameState.IState? currentState, IEventEnvelope envelope, RequestType type)
         {
+            if (currentState == null)
+                return;
+
             foreach (var t in DataTrackers)
                 t.Request(currentState, TrackerState, envelope, type);
         }

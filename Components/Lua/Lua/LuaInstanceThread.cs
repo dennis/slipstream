68c9fc54a77b4d24faa5b7f158eb211ef5c517db
Dennis Møllegaard <dennis@moellegaard.dk>
2021-09-28 20:12:08 +0200

    Bugfix: generate_json() must not be static
    otherwise NLua can't use it

--- Components/Lua/Lua/LuaInstanceThread.cs
+++ Components/Lua/Lua/LuaInstanceThread.cs
@@ -13,8 +13,6 @@ using Slipstream.Shared;
 using Slipstream.Shared.Helpers.StrongParameters;
 using Slipstream.Shared.Lua;
 
-using Swan.Formatters;
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -312,7 +310,7 @@ end
         }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "This is expose in Lua, so we want to keep that naming style")]
-        public static string generate_json(LuaTable luaTable)
+        public string generate_json(LuaTable luaTable)
         {
             return JsonConvert.SerializeObject(Parameters.From(luaTable));
         }

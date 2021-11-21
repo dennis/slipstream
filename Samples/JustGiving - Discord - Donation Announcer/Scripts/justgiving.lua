f979d99b0c39e6ee00319cd96eb6c0bc69303491
Dennis Møllegaard Pedersen <dennis@moellegaard.dk>
2021-11-21 12:32:08 +0100

    LuaSample: justgiving.lua

---
+++ Samples/JustGiving - Discord - Donation Announcer/Scripts/justgiving.lua
@@ -0,0 +1,29 @@
+﻿local cfg = require("lib/config")
+local jg = require("api/justgiving"):instance(cfg.justgiving)
+local discord = require("api/discord"):instance(cfg.discord)
+
+--jg:send_all_donations()
+
+addEventHandler("JustGivingDonation", function(event)
+    local icon = ":currency_exchange:"
+
+    if event.Amount >= 100 then
+        icon = ":star_struck:"
+    elseif event.Amount >= 50 then
+        icon = ":star2:"
+    elseif event.Amount >= 20 then
+        icon = ":star:"
+    end
+
+    if event.DonorLocalCurrencyCode == event.CurrencyCode then
+        discord:channel_id(cfg.ch_donations):send_message(icon .. " __" .. event.DonorDisplayName .. "__ : " .. event.DonorLocalAmount .. " " ..  event.DonorLocalCurrencyCode .. " " ..  event.Message)
+    else
+        discord:channel_id(cfg.ch_donations):send_message(icon .. " __" .. event.DonorDisplayName .. "__ : " .. event.DonorLocalAmount .. " " ..  event.DonorLocalCurrencyCode .. " (~ " .. event.Amount .. " " .. event.CurrencyCode .. ") " ..  event.Message)
+    end    
+end)
+
+
+addEventHandler("JustGivingInfo", function(event)
+    discord:channel_id(cfg.ch_donations):send_message("**Donation Total**: " .. event.FundraisingGrandTotal .. " / " .. event.FundraisingTarget .. " " .. event.CurrencyCode)
+end)
+ 
